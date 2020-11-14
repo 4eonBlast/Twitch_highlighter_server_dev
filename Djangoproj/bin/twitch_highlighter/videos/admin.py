6206a8d417816d506869291bb717e28b23d9d5ac
4eonBlast <bounce1011@naver.com>
2020-11-14 16:27:54 +0900

    video filter add

--- Djangoproj/bin/twitch_highlighter/videos/admin.py
+++ Djangoproj/bin/twitch_highlighter/videos/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Streamer, Videos
+from .models import Streamer, Videos, Comment
 # Register your models here.
 
 admin.site.register(Streamer)
-admin.site.register(Videos)
+# admin.site.register(Videos)
+
+
+@admin.register(Videos)
+class VideosAdmin(admin.ModelAdmin):
+    list_display = ['streamer_name', 'vid_num', 'registered_dttm']
+    list_filter = ['streamer_name']
+
+
+@admin.register(Comment)
+class CoomentAdmin(admin.ModelAdmin):
+    list_filter = ['post']
