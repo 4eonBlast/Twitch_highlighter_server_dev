a40e84f35e021e229b4a63f3132d6a79a2ff5cfe
4eonBlast <bounce1011@naver.com>
2020-11-21 18:16:43 +0900

    homepage add

--- Djangoproj/bin/twitch_highlighter/users/urls.py
+++ Djangoproj/bin/twitch_highlighter/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import home, register
+from .views import home, register, search
 urlpatterns = [
     path('register/', register, name='register'),
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('search/', search, name='video_search')
 ]
--- Djangoproj/bin/twitch_highlighter/users/views.py
+++ Djangoproj/bin/twitch_highlighter/users/views.py
@@ -1,11 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from videos.models import Videos, Streamer
+from django.db.models import Count
+from django.core.paginator import Paginator
+from videos.views import video_filt_by_streamer
 # Create your views here.
 
 from .forms import RegisterForm
 
 
+def search(request):
+
+    v = request.GET.get('v', '')
+    if v:
+        search = Streamer.objects.filter(streamer_name=v)
+        if search.exists():
+            search = Streamer.objects.get(streamer_name=v)
+            return video_filt_by_streamer(request, search.id)
+
+    sort_by_like = Videos.objects.annotate(
+        like_count=Count('likes')).order_by('-like_count')
+
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(sort_by_like, 10)
+    like_videos = paginator.get_page(page)
+
+    latest = Videos.objects.all().order_by('-registered_dttm')
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(latest, 10)
+    latest_vid = paginator.get_page(page)
+    error = "no such streamer"
+    return render(request, 'registration/home.html', context={'like_videos': like_videos, 'latest_videos': latest_vid, 'error': error})
+
+
 def home(request):
-    return render(request, 'registration/home.html')
+
+    sort_by_like = Videos.objects.annotate(
+        like_count=Count('likes')).order_by('-like_count')
+
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(sort_by_like, 10)
+    like_videos = paginator.get_page(page)
+
+    latest = Videos.objects.all().order_by('-registered_dttm')
+    page = int(request.GET.get('p', 1))
+    paginator = Paginator(latest, 10)
+    latest_vid = paginator.get_page(page)
+
+    return render(request, 'registration/home.html', context={'like_videos': like_videos, 'latest_videos': latest_vid})
 
 
 def register(request):
